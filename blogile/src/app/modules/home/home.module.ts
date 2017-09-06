b778801cdfc653d09bca2c3baeb1f9920634aed1
LammaSF <lilibabacheva@yahoo.com>
2017-09-07 00:13:35 +0300

    statistics module with button on home page

--- blogile/src/app/modules/home/home.module.ts
+++ blogile/src/app/modules/home/home.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SideNavModule } from './side-nav/side-nav.module';
+import { StatisticsModule} from "../statistics/statistics.module";
+
 
 @NgModule({
   imports: [
     CommonModule,
-    SideNavModule
+    SideNavModule,
+    StatisticsModule
   ],
   declarations: [HomeComponent]
 })
--- blogile/src/app/modules/routes/routes.module.ts
+++ blogile/src/app/modules/routes/routes.module.ts
@@ -5,16 +5,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from '../../app.component';
 import { UserModule } from '../user/user.module';
+import { StatisticsModule} from "../statistics/statistics.module";
 
 import { SignInComponent } from '../sign-in/sign-in.component';
 import { SignUpComponent } from '../sign-up/sign-up.component';
-import {HomeComponent} from "../home/home.component";
+import { HomeComponent } from "../home/home.component";
+import { StatisticsComponent} from '../statistics/statistics.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'sign-in', component: SignInComponent },
-  { path: 'sign-up', component: SignUpComponent }
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'statistics', component: StatisticsComponent}
 
   ];
 
