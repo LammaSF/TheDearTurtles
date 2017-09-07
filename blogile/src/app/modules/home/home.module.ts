d321b53f6a8ac094bfe613212bafcda799590c97
LammaSF <lilibabacheva@yahoo.com>
2017-09-07 13:21:33 +0300

    statistics table

--- blogile/src/app/modules/home/home.module.ts
+++ blogile/src/app/modules/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SideNavModule } from './side-nav/side-nav.module';
 import { StatisticsModule} from "../statistics/statistics.module";
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     SideNavModule,
     StatisticsModule
   ],
